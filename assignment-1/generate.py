8572952493cdc42a8c419f3676dc3048ed3a7e6f
Arpit Roopchandani <arpit9295@gmail.com>
2021-02-16 22:53:32 +0000

    Update generate.py to support FNN

--- assignment-1/generate.py
+++ assignment-1/generate.py
@@ -26,6 +26,8 @@ parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
+parser.add_argument('--prompt', type=str,
+                    help="input prompt, if any")
 parser.add_argument('--temperature', type=float, default=1.0,
                     help='temperature - higher will increase diversity')
 parser.add_argument('--log-interval', type=int, default=100,
@@ -45,34 +47,36 @@ if args.temperature < 1e-3:
 
 with open(args.checkpoint, 'rb') as f:
     model = torch.load(f).to(device)
-model.eval()
 
 corpus = data.Corpus(args.data)
 ntokens = len(corpus.dictionary)
 
-is_transformer_model = hasattr(model, 'model_type') and model.model_type == 'Transformer'
-if not is_transformer_model:
-    hidden = model.init_hidden(1)
-input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
+if args.prompt:
+    prompt = args.prompt.split()
+    input = torch.tensor([], dtype=torch.long).to(device)
+    for i in range(0, len(prompt)):
+        input = torch.cat((input, torch.tensor([corpus.dictionary.word2idx[prompt[i]]]).to(device)))
+    input = input[:model.seq_size].view(1, -1)
+else:
+    input = torch.randint(ntokens, (1, model.seq_size), dtype=torch.long).to(device)
 
 with open(args.outf, 'w') as outf:
+    outf.write('==== Prompt ==========================\n' )
+    for i in range(0, input.size(1)):
+        outf.write(corpus.dictionary.idx2word[input[0][i]] + ' ')
+    outf.write('\n\n==== Output ==========================\n\n' )
+    for i in range(0, input.size(1)):
+        outf.write(corpus.dictionary.idx2word[input[0][i]] + ' ')
     with torch.no_grad():  # no tracking history
         for i in range(args.words):
-            if is_transformer_model:
-                output = model(input, False)
-                word_weights = output[-1].squeeze().div(args.temperature).exp().cpu()
-                word_idx = torch.multinomial(word_weights, 1)[0]
-                word_tensor = torch.Tensor([[word_idx]]).long().to(device)
-                input = torch.cat([input, word_tensor], 0)
-            else:
-                output, hidden = model(input, hidden)
-                word_weights = output.squeeze().div(args.temperature).exp().cpu()
-                word_idx = torch.multinomial(word_weights, 1)[0]
-                input.fill_(word_idx)
+            output = model(input)
+            word_weights = output.squeeze().div(args.temperature).exp().cpu()
+            word_idx = torch.multinomial(word_weights, 1)[0]
+            input = torch.cat((input.view(-1)[1:], torch.tensor([word_idx]).to(device))).view(1, -1)
 
             word = corpus.dictionary.idx2word[word_idx]
 
-            outf.write(word + ('\n' if i % 20 == 19 else ' '))
+            outf.write('\n' if word == '<eos>' else word + ' ')
 
             if i % args.log_interval == 0:
                 print('| Generated {}/{} words'.format(i, args.words))
